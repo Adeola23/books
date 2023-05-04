5c41aea92c4406948a5b4acd33fd9fdb64277748
Adeola23 <amosadeola92@gmail.com>
2023-05-04 11:11:41 +0100

    update

--- src/main/java/com/example/assignmen_4/Credentials.java
+++ src/main/java/com/example/assignmen_4/Credentials.java
@@ -2,10 +2,10 @@ package com.example.assignmen_4;
 
 public class Credentials {
     //JDBC connection
-    public static final String USERNAME = "adeolaabiola";
-    public static final String PASSWORD = "atlas123";
-    public static final String URL = "jdbc:postgresql://localhost:5432/FSAD2023_Books";
+    public static final String USERNAME = "";
+    public static final String PASSWORD = "";
+    public static final String URL = "";
     //Client-server connection
-    public static final String HOST = "127.0.0.1"; //localhost
+    public static final String HOST = ""; //localhost
     public static final int PORT = 9994; //This is NOT the port in postgres, but the port at which the BooksDatabaseServer is listening
 }
